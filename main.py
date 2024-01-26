89f83cfaa25ccba6ed050faa06111385d6888dff
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-01-26 14:11:17 +0200

    .

---
+++ main.py
@@ -0,0 +1,18 @@
+from currency_converter import CurrencyConverter
+from datetime import date
+import tkinter as tk
+
+
+root = tk.Tk()
+root.geometry("500x500")
+root.title("Currency converter")
+root.mainloop()
+
+
+# c = CurrencyConverter()
+#
+# print(c.convert(100, "EUR", "RON", date=date(2024, 1, 2)))
+
+
+
+
