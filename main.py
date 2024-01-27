91bc63958c8051e6e4e131c18483d8b5dc9cb65b
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-01-27 12:46:54 +0200

    .

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ class Converter:
     def __init__(self):
         self.root = tk.Tk()
 
-        self.convert_button = tk.Button(self.root, text="Convert", font=("Arial", 16),command=self.exchange)
+        self.convert_button = tk.Button(self.root, text="Convert", font=("Arial", 16), command=self.exchange)
         self.convert_button.pack(padx=10, pady=10)
+
+        self.result_label = tk.Label(self.root, text="", font=("Arial", 16))
+        self.result_label.pack(pady=10)
 
         self.root.mainloop()
 
     def exchange(self):
-        print(c.convert(100, "RON", "EUR", date=date(2024, 1, 2)))
+        result = c.convert(100, "RON", "EUR", date=date(2024, 1, 2))
+        result_round = round(result)
+        self.result_label.config(text=f"{result_round}")
 
 Converter()
 
