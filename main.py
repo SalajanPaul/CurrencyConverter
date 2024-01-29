0f863916e0108aa9a128ebdc8bfc5824424b7d2b
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-01-29 11:57:29 +0200

    V1

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ class Converter:
         self.root = tk.Tk()
         self.root.title("Exchange")
 
+        options_list1 = ["RON", "EUR", "RUB", "USD"]
+        options_list2 = ["1", "2", "3", "4"]
+
+        value_inside = tk.StringVar(self.root)
+        value_inside.set("Select an Option")
+
+        question_meniu1 = tk.OptionMenu(self.root, value_inside, *options_list1)
+        question_meniu1.pack()
+
+        question_meniu2 = tk.OptionMenu(self.root, value_inside, *options_list2)
+        question_meniu2.pack()
+
         self.convert_button = tk.Button(self.root, text="Convert", font=("Arial", 16), command=self.exchange)
         self.convert_button.pack(padx=10, pady=10)
 
